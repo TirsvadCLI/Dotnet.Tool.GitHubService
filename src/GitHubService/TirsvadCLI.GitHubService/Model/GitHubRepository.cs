046e112096b2b504e4d5eff5d3f7eb45d3ab80e0
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-04-11 04:49:53 +0800

    Fix json compatible

--- src/GitHubService/TirsvadCLI.GitHubService/Model/GitHubRepository.cs
+++ src/GitHubService/TirsvadCLI.GitHubService/Model/GitHubRepository.cs
@@ -1,8 +1,12 @@
-﻿namespace TirsvadCLI.GitHubService.Model;
+﻿using System.Text.Json.Serialization;
+
+namespace TirsvadCLI.GitHubService.Model;
 
 public class GitHubRepository
 {
     public string? Name { get; set; } ///> The name of the repository.
+    [JsonPropertyName("html_url")]
     public string? HtmlUrl { get; set; } ///> The URL to the repository on GitHub.
+    [JsonPropertyName("clone_url")]
     public string? CloneUrl { get; set; } ///> The URL to clone the repository.
 }
